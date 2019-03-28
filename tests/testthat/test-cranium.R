d21c9459ff6dbf5c6dd823608df5006417bb4785
Ben Baumer <ben.baumer@gmail.com>
2019-03-28 10:33:34 -0400

    added test

--- tests/testthat/test-cranium.R
+++ tests/testthat/test-cranium.R
@@ -1,5 +1,14 @@
 context("test-cranium")
 
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("reading works", {
+  file <- "~/Data/barresi/AT_1_Probabilities.h5"
+  if (file.exists(file)) {
+    if (require(dplyr)) {
+      brain <- read_h5(file)
+      expect_is(brain, "brain")
+      tidy_brain <- tidy(brain)
+      expect_is(tidy_brain, "tbl_brain", "tbl_df")
+      expect_equal(ncol(tidy_brain), 5)
+    }
+  }
 })
